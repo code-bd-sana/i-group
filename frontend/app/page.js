12c2fc3cf113d389df35c6df532c14a6520da99b
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-11-22 14:29:08 +0600

    elisa

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,7 @@
-import Activities from "@/components/dashboard/Activities.jsx";
-import DashboardChart from "@/components/dashboard/DashboardChart.jsx";
-import DashboardHeader from "@/components/dashboard/DashboardHeader.jsx";
-import RecentUploads from "@/components/dashboard/RecentUploads.jsx";
+import Activities from "../components/dashboard/Activities";
+import DashboardChart from "../components/dashboard/DashboardChart";
+import DashboardHeader from "../components/dashboard/DashboardHeader";
+import RecentUploads from "../components/dashboard/RecentUploads.jsx";
 
 //hello buff
 
